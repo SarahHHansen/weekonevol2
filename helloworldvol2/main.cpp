c99ac1926f8ead364ba63bcd1f940dc79fe2c2a5
SarahHHansen <Sarah@52d3ca5d.dynamic-ip.k-net.dk>
2017-04-23 21:02:02 +0200

    hej

---
+++ helloworldvol2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworldvol2
+//
+//  Created by Sarah Husum Hansen on 23/04/2017.
+//  Copyright © 2017 Sarah Husum Hansen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
